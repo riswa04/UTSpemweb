6852866ee10191a39004304402b655a27cac0f10
risnawati <risnawa417@gmail.com>
2025-05-06 10:50:51 +0700

    Add files via upload

---
+++ koreksi.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($_POST['nama']) && !empty($_POST['nama'])) {
+    $nama = htmlspecialchars($_POST['nama']);
+    echo "Halo, " . $nama;
+} else {
+    echo "Nama belum di isi.";
+}
+?>
